db6e4f5a7e4fdf582dfc3dfa7fc0be1a6137a678
mysh <mysh995@gmail.com>
2025-10-29 17:38:12 +0000

    Policy class

--- src/rltoolbox/policy.py
+++ src/rltoolbox/policy.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 
 
 class Action:
@@ -5,6 +6,9 @@ class Action:
         self._name = name
         self._expected_reward = initial_expected_reward
         self._times_taken = 0
+
+    def __str__(self) -> str:
+        return f"Action '{self._name}', taken {self._times_taken} times, expected reward: {self._expected_reward}"
 
     @property
     def name(self) -> str:
@@ -27,3 +31,19 @@ class Action:
     def update(self, received_reward: float):
         self._times_taken += 1
         self._expected_reward = self._expected_reward + self.step_size * (received_reward - self._expected_reward)
+
+
+class Policy:
+    def __init__(self, action_names: Iterable[str], initial_expected_reward: float = 0):
+        self._actions = tuple(Action(name, initial_expected_reward) for name in action_names)
+
+    @property
+    def actions(self) -> tuple[Action, ...]:
+        return self._actions
+
+
+if __name__ == '__main__':
+    p = Policy([str(i) for i in range(5)])
+    for a in p.actions:
+        print(a)
+
