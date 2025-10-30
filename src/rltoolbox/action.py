383b939e731dad016359504f7853c646d18623b3
mysh <mysh995@gmail.com>
2025-10-30 12:43:27 +0000

    Some refactoring

---
+++ src/rltoolbox/action.py
@@ -0,0 +1,8 @@
+class Action:
+    def __init__(self, name: str, average_reward: float):
+        self._name = name
+        self._average_reward = average_reward
+
+    @property
+    def name(self) -> str:
+        return self._name
--- src/rltoolbox/policy.py
+++ src/rltoolbox/policy.py
@@ -5,51 +5,11 @@ import logging
 from math import sqrt, log
 from typing import Callable
 
+from rltoolbox.action import Action
+from rltoolbox.action_estimate import ActionEstimate
+
 
 logger = logging.getLogger(__name__)
-
-
-class Action:
-    def __init__(self, name: str, average_reward: float):
-        self._name = name
-        self._average_reward = average_reward
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-
-class ActionEstimate:
-    def __init__(self, action: Action, initial_expected_reward: float = 0.):
-        self._action = action
-        self._expected_reward = initial_expected_reward
-        self._times_taken = 0
-
-    @property
-    def action(self) -> Action:
-        return self._action
-
-    @property
-    def action_name(self) -> str:
-        return self._action.name
-
-    @property
-    def expected_reward(self) -> float:
-        return self._expected_reward
-
-    @property
-    def times_taken(self) -> int:
-        return self._times_taken
-
-    @property
-    def step_size(self) -> float:
-        if not self._times_taken:
-            raise RuntimeError("Step size not defined before action is taken at least once")
-        return 1 / self._times_taken
-
-    def update(self, received_reward: float):
-        self._times_taken += 1
-        self._expected_reward = self._expected_reward + self.step_size * (received_reward - self._expected_reward)
 
 
 class Policy(ABC):
