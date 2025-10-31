e7a0bddf5210bee0d43982dd0214ef279cd0f6b6
mysh <mysh995@gmail.com>
2025-10-31 18:28:16 +0000

    Method to evaluate policy

--- tasks/gambler_problem/utils.py
+++ tasks/gambler_problem/utils.py
@@ -12,6 +12,8 @@ class GamblerProblemModel:
         self.immediate_rewards = np.zeros(self.n_states)
         self.immediate_rewards[-1] = 1
 
+        self.discount = 1
+
     def propose_policy_estimate(self) -> npt.NDArray[np.integer]:
         return np.ones(self.n_nonterminal_states, dtype=int)
 
@@ -41,3 +43,39 @@ class GamblerProblemModel:
 
         if not np.issubdtype(policy_estimate.dtype, np.integer):
             raise TypeError(f"Expected an integer-valued array, got {policy_estimate.dtype}")
+
+    def evaluate_policy(
+            self,
+            policy: npt.NDArray[np.integer],
+            n_steps: int,
+            initial_state_values: npt.NDArray[np.floating] | None = None,
+            keep_track: bool = False
+    ) -> npt.NDArray[np.floating]:
+
+        if initial_state_values is None:
+            state_values = np.zeros(self.n_states)
+        else:
+            if initial_state_values.shape != self.n_states:
+                raise ValueError(f"Initial values array must have shape {(self.n_states,)}")
+            state_values = initial_state_values
+
+        if keep_track:
+            track = np.empty((n_steps+1, self.n_states), dtype=state_values.dtype)
+            track[0] = state_values
+        else:
+            track = None
+
+        r = self.immediate_rewards
+        ptpm = self.define_ptpm(policy)
+        d = self.discount
+
+        for i in range(10):
+            new_values_of_states = r + d * np.matmul(ptpm, state_values)
+            new_values_of_states = np.round(new_values_of_states, 4)
+            if keep_track:
+                track[i+1] = new_values_of_states
+            state_values = new_values_of_states
+
+        if keep_track:
+            return track
+        return state_values
